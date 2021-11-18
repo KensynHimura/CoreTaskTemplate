6050ba3355430bdc01cc7659bdc41d4cdcde97d0
KensynHimura <100blowsofhearth@gmail.com>
2021-11-18 18:47:13 +1000

    4rd update

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,19 +1,14 @@
 package jm.task.core.jdbc;
-
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 
 public class Main {
     public static void main(String[] args){
-
-     UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
+        UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
         userDaoJDBC.createUsersTable();
         userDaoJDBC.saveUser("Lwrw", "Alewrvich", (byte)24);
         userDaoJDBC.saveUser("QRQrq", "Aqr3y", (byte)29);
-//        userDaoJDBC.getAllUsers();
-//        userDaoJDBC.cleanUsersTable();
-//        userDaoJDBC.dropUsersTable();
-
-
-
+        userDaoJDBC.getAllUsers();
+        userDaoJDBC.cleanUsersTable();
+        userDaoJDBC.dropUsersTable();
     }
 }
