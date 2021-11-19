4bfeb6d1c20596ad1da21614400a54116ad1b0a9
KensynHimura <100blowsofhearth@gmail.com>
2021-11-20 00:46:26 +1000

    2nd update

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -2,6 +2,8 @@ package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 import java.util.List;
 
@@ -26,27 +28,46 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-        util.getFactory().getCurrentSession().beginTransaction();
-        util.getFactory().getCurrentSession().save(new User(name, lastName, age));
-        util.getFactory().getCurrentSession().getTransaction().commit();
-        System.out.println("Миньён c именем " + name + " добавлен в таблицу");
+        try {
+            util.getFactory().getCurrentSession().beginTransaction();
+            util.getFactory().getCurrentSession().save(new User(name, lastName, age));
+            util.getFactory().getCurrentSession().getTransaction().commit();
+            System.out.println("Миньён c именем " + name + " добавлен в таблицу");
+        } catch (Exception e) {
+            e.printStackTrace();
+            util.getFactory().getCurrentSession().getTransaction().rollback();
+        }
+
 
     }
 
     @Override
     public void removeUserById(long id) {
-        util.getFactory().getCurrentSession().beginTransaction();
-        util.getFactory().getCurrentSession().delete(util.getFactory().getCurrentSession().get(User.class, id));
-        util.getFactory().getCurrentSession().getTransaction().commit();
-        System.out.println("Миньён под номером :" + id + " был удален.");
+        try {
+            util.getFactory().getCurrentSession().beginTransaction();
+            util.getFactory().getCurrentSession().delete(util.getFactory().getCurrentSession().get(User.class, id));
+            util.getFactory().getCurrentSession().getTransaction().commit();
+            System.out.println("Миньён под номером :" + id + " был удален.");
+        } catch (Exception e) {
+            e.printStackTrace();
+            util.getFactory().getCurrentSession().getTransaction().rollback();
+        }
+
     }
 
     @Override
     public List<User> getAllUsers() {
-        util.getFactory().getCurrentSession().beginTransaction();
-        List<User> minions = util.getFactory().getCurrentSession().createQuery("from User").getResultList();
-        util.getFactory().getCurrentSession().getTransaction().commit();
-        System.out.println(minions);
+        List<User> minions = null;
+        try {
+            util.getFactory().getCurrentSession().beginTransaction();
+            minions = util.getFactory().getCurrentSession().createQuery("from User").getResultList();
+            util.getFactory().getCurrentSession().getTransaction().commit();
+            System.out.println(minions);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            util.getFactory().getCurrentSession().getTransaction().rollback();
+        }
         return minions;
     }
 
